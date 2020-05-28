9997d322f6c9e8d2636762f4daf3105086ad0220
Khoa <ngkhoa97@gmail.com>
2020-05-28 20:58:10 +0700

    them connect DB

--- config/Config.js
+++ config/Config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  DB: 'mongodb://localhost:27017/movie_rating_app',
+  // DB: 'mongodb://localhost:27017/movie_rating_app',
+  DB: 'mongodb://ngkhoa97:xXIjovNRBo6KPXHh@cluster0-shard-00-00-up7kl.gcp.mongodb.net:27017,cluster0-shard-00-01-up7kl.gcp.mongodb.net:27017,cluster0-shard-00-02-up7kl.gcp.mongodb.net:27017/movie_rating_app?replicaSet=Cluster0-shard-0&authSource=admin&ssl=true',
   // SECRET: 'movieratingappsecretkey',
   SECRET: 'thisisthesecretkey',
   FACEBOOK_APP_ID: '1130679753965897',
